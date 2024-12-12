2c7d10f946a3b6659e21b83aa85f028d233894d3
tanay0209 <tanayjagnani@gmail.com>
2024-12-12 12:43:55 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='min-h-dvh w-full flex items-center justify-center'>
+      <h1 className='font-black text-6xl'>
+        This is a starter kit
+      </h1>
+    </div>
+  )
+}
+
+export default App
